14f607d1463595535b73b89e9ed15f84febdbe97
Falk Wilke <fwilke@uos.de>
2016-02-18 11:10:28 +0100

    added top level logic

---
+++ ModulflexDesignerTool/src/main/java/de/uos/se/designertool/logic/ModulflexDesignerLogic.java
@@ -0,0 +1,28 @@
+package de.uos.se.designertool.logic;
+
+import de.uos.se.designertool.logic.nodeserverlogic.ModulflexNodeServer;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+/**
+ * Created by sem on 18.02.2016.
+ */
+public class ModulflexDesignerLogic
+{
+    private final ObjectProperty<ModulflexNodeServer> _ns;
+
+    public ModulflexDesignerLogic()
+    {
+        this(null);
+    }
+
+    public ModulflexDesignerLogic(ModulflexNodeServer ns)
+    {
+        this._ns = new SimpleObjectProperty<>(ns);
+    }
+
+    public ObjectProperty<ModulflexNodeServer> nodeServerProperty()
+    {
+        return _ns;
+    }
+}
--- ModulflexDesignerTool/src/main/java/de/uos/se/designertool/logic/nodeserverlogic/ModulflexNodeServer.java
+++ ModulflexDesignerTool/src/main/java/de/uos/se/designertool/logic/nodeserverlogic/ModulflexNodeServer.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 /**
  * Created by sem on 18.02.2016.
  */
-public class ModulflexNoderServer
+public class ModulflexNodeServer
 {
     private final DoubleProperty cycleTime;
 
@@ -38,7 +38,7 @@ public class ModulflexNoderServer
      * @param children
      *         A list of children. Not required.
      */
-    private ModulflexNoderServer(double cycleTime, Boolean profiling, String logDir, String logLevel, Boolean noLog, List<ModulflexNode> children)
+    private ModulflexNodeServer(double cycleTime, Boolean profiling, String logDir, String logLevel, Boolean noLog, List<ModulflexNode> children)
     {
         this.cycleTime = new SimpleDoubleProperty(cycleTime);
 
@@ -112,6 +112,18 @@ public class ModulflexNoderServer
     }
 
     @Override
+    public int hashCode()
+    {
+        int result = cycleTime.hashCode();
+        result = 31 * result + profiling.hashCode();
+        result = 31 * result + logDir.hashCode();
+        result = 31 * result + logLevel.hashCode();
+        result = 31 * result + noLog.hashCode();
+        result = 31 * result + children.hashCode();
+        return result;
+    }
+
+    @Override
     public boolean equals(Object o)
     {
         if (this == o)
@@ -119,7 +131,7 @@ public class ModulflexNoderServer
         if (o == null || getClass() != o.getClass())
             return false;
 
-        ModulflexNoderServer that = (ModulflexNoderServer) o;
+        ModulflexNodeServer that = (ModulflexNodeServer) o;
 
         if (! cycleTime.equals(that.cycleTime))
             return false;
@@ -136,21 +148,9 @@ public class ModulflexNoderServer
     }
 
     @Override
-    public int hashCode()
-    {
-        int result = cycleTime.hashCode();
-        result = 31 * result + profiling.hashCode();
-        result = 31 * result + logDir.hashCode();
-        result = 31 * result + logLevel.hashCode();
-        result = 31 * result + noLog.hashCode();
-        result = 31 * result + children.hashCode();
-        return result;
-    }
-
-    @Override
     public String toString()
     {
-        return "ModulflexNoderServer{" +
+        return "ModulflexNodeServer{" +
                 "cycleTime=" + cycleTime +
                 ", profiling=" + profiling +
                 ", logDir=" + logDir +
