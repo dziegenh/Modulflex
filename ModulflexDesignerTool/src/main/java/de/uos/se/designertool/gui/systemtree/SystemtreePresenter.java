9013528ab93333db81df0cc32855975e8e5a81e2
Falk Wilke <fwilke@uos.de>
2016-02-18 14:13:06 +0100

    finished draft

--- ModulflexDesignerTool/src/main/java/de/uos/se/designertool/gui/systemtree/SystemtreePresenter.java
+++ ModulflexDesignerTool/src/main/java/de/uos/se/designertool/gui/systemtree/SystemtreePresenter.java
@@ -1,18 +1,22 @@
 package de.uos.se.designertool.gui.systemtree;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import de.uos.se.designertool.logic.nodeserverlogic.ModulflexModule;
+import de.uos.se.designertool.logic.nodeserverlogic.ModulflexNode;
+import de.uos.se.designertool.logic.nodeserverlogic.ModulflexNodeServer;
+import de.uos.se.designertool.logic.nodeserverlogic.ModulflexSystemElementType;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
 
-import javax.inject.Inject;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author dziegenhagen
@@ -24,39 +28,66 @@ public class SystemtreePresenter
     @FXML
     AnchorPane rootPane;
 
-    TreeView treeView;
+    TreeView<ModulflexSystemElementType> treeView;
 
-    @Inject
-//    DummyLogic someLogic;
+    ObjectProperty<ModulflexNodeServer> ns;
+
+    ListProperty<ModulflexNode> children;
+
+    ObjectProperty<ModulflexModule> current;
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
 
         // dummy entries
-        TreeItem<String> rootItem = new TreeItem<String>("Dummy");
+        ns = new SimpleObjectProperty<>();
+        children = new SimpleListProperty<>();
+        current = new SimpleObjectProperty<>();
+        TreeItem<ModulflexSystemElementType> rootItem = new TreeItem<>();
         rootItem.setExpanded(true);
-        for (int i = 1; i < 6; i++)
-        {
-            TreeItem<String> item = new TreeItem<String>("Sub Dummy " + i);
-            rootItem.getChildren().add(item);
-        }
-
-        treeView = new TreeView<>(rootItem);
-
-        treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem>()
-        {
-
-            @Override
-            public void changed(ObservableValue<? extends TreeItem> observable, TreeItem oldValue, TreeItem newValue)
-            {
-                Logger.getLogger(SystemtreePresenter.class.getName())
-                      .log(Level.INFO, "Tree selection changed: {0}", newValue);
-//                someLogic.doSomething();
-            }
-
+        ns.addListener((observable1, oldValue1, newValue1) -> {
+            load(newValue1);
         });
+        treeView = new TreeView<>(rootItem);
+        children.addListener((observable, oldValue, newValue) -> {
+            rootItem.getChildren().clear();
+            for (ModulflexNode node : newValue)
+            {
+                TreeItem<ModulflexSystemElementType> nested = new TreeItem<>(node);
+                rootItem.getChildren().add(nested);
+                for (ModulflexModule module : node.modulesProperty())
+                {
+                    nested.getChildren().add(new TreeItem<>(module));
+                }
+            }
+        });
+
+        treeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            ModulflexSystemElementType value = newValue.getValue();
+            System.out.println(value);
+            if (value instanceof ModulflexModule)
+                current.setValue((ModulflexModule) value);
+        });
+
+
+        treeView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         rootPane.getChildren().add(treeView);
     }
 
+    private void load(ModulflexNodeServer newValue)
+    {
+        children.unbind();
+        children.bindBidirectional(newValue.childrenProperty());
+    }
+
+    public ObjectProperty<ModulflexNodeServer> nodeServerProperty()
+    {
+        return ns;
+    }
+
+    public ObjectProperty<ModulflexModule> currentProperty()
+    {
+        return current;
+    }
 }
