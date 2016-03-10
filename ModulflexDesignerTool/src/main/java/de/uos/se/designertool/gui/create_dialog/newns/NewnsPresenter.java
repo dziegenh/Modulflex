30b9c79a161a928ab0c370ed687f309f78ce9217
jps <jschleutker@uos.de>
2016-03-10 14:01:18 +0100

    changed two properties of the nameserver to checkboxes.

--- ModulflexDesignerTool/src/main/java/de/uos/se/designertool/gui/create_dialog/newns/NewnsPresenter.java
+++ ModulflexDesignerTool/src/main/java/de/uos/se/designertool/gui/create_dialog/newns/NewnsPresenter.java
@@ -8,7 +8,7 @@ import de.uos.se.designertool.logic.NodeChangedModule;
 import de.uos.se.designertool.logic.NodeServerAddedModule;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.util.converter.NumberStringConverter;
 
@@ -36,13 +36,13 @@ public class NewnsPresenter
     @FXML
     private TextField cycleTime;
     @FXML
-    private ComboBox<?> profilingBox;
+    private CheckBox profilingBox;
     @FXML
     private TextField logDir;
     @FXML
     private TextField logLevel;
     @FXML
-    private ComboBox<?> noLog;
+    private CheckBox noLog;
     private ModulflexNodeServer nodeServer;
     private int globalID;
 
@@ -54,10 +54,10 @@ public class NewnsPresenter
             nodeServer = data;
             cycleTime.textProperty().bindBidirectional(data.cycleTimeProperty(), new NumberStringConverter());
             data.nameProperty().bindBidirectional(nameField.textProperty());
-            data.profilingProperty().bind(profilingBox.getSelectionModel().selectedItemProperty().isEqualTo("true"));
+            data.profilingProperty().bindBidirectional(profilingBox.selectedProperty());
             data.logDirProperty().bindBidirectional(logDir.textProperty());
             data.logLevelProperty().bindBidirectional(logLevel.textProperty());
-            data.noLogProperty().bind(noLog.getSelectionModel().selectedItemProperty().isEqualTo("true"));
+            data.noLogProperty().bindBidirectional(noLog.selectedProperty());
         });
     }
 
